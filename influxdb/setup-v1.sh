e8fa883e650b94bbe5ae5f3ca421a8d1f381323f
Vladimir Bogdanov <vld@bgdn.io>
2022-08-15 22:04:49 +0300

    test fixes

--- influxdb/setup-v1.sh
+++ influxdb/setup-v1.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-#echo "Adding DBRP mapping"
-#echo "BUCKET_ID = " ${DOCKER_INFLUXDB_INIT_BUCKET_ID}
-#echo "DB_NAME = " ${DB_NAME}
-
 influx v1 dbrp create \
   --bucket-id ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
-  --db ${DB_NAME} \
-  --rp ${RP_NAME} \
+  --db ${V1_DB_NAME} \
+  --rp ${V1_RP_NAME} \
   --default \
+  --org ${DOCKER_INFLUXDB_INIT_ORG}
+
+influx v1 auth create \
+  --username ${V1_AUTH_USERNAME} \
+  --password ${V1_AUTH_PASSWORD} \
+  --write-bucket ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
   --org ${DOCKER_INFLUXDB_INIT_ORG}
